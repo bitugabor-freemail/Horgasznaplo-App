c7162940e077fd4ee8b035bddebcafdb513c01c9
bitugabor <83548210+bitugabor@users.noreply.github.com>
2026-08-07 13:47:08 +0200

    Update fogasok.dart

--- lib/fogasok.dart
+++ lib/fogasok.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'image_picker/image_picker.dart';
+import 'package:image_picker/image_picker.dart';
 import 'adattarolo.dart';
 import 'modellek.dart';
 import 'torzsadatok.dart';
