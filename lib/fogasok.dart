04087d0a90ff159c81f05bfb7cef437495fa7bef
bitugabor <83548210+bitugabor@users.noreply.github.com>
2026-08-16 01:10:38 +0200

    Update fogasok.dart

--- lib/fogasok.dart
+++ lib/fogasok.dart
@@ -97,10 +97,10 @@ class _FogasokScreenState extends State<FogasokScreen> {
   }
 
   Color _getKartyaszin(String? sors) {
-    if (sors == 'Visszaengedtem') return Colors.green.withOpacity(0.25);
-    if (sors == 'Elvittem') return Colors.blue.withOpacity(0.25);
-    if (sors == 'Elajándékoztam') return Colors.orange.withOpacity(0.25);
-    if (sors == 'Elpusztult') return Colors.red.withOpacity(0.25);
+    if (sors == 'Visszaengedtem') return Colors.green.withOpacity(0.18);
+    if (sors == 'Elvittem') return Colors.blue.withOpacity(0.18);
+    if (sors == 'Elajándékoztam') return Colors.orange.withOpacity(0.18);
+    if (sors == 'Elpusztult') return Colors.red.withOpacity(0.18);
     return const Color(0xFF1E1E1E); 
   }
 
